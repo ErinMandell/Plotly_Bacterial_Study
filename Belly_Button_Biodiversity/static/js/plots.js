0790713a8a99d4b9c90b7ea3f9e96b968503e3bc
ErinMandell <ErinMandell@users.noreply.github.com>
2020-01-13 17:04:15 -0600

    updating app.js

--- Belly_Button_Biodiversity/static/js/plots.js
+++ Belly_Button_Biodiversity/static/js/plots.js
@@ -1,44 +1,54 @@
 
-function DrawBargraph(sampleID)
-{
-    console.log("DrawBargraph: sample = ", sampleID);
-}
+// function DrawBargraph(sampleID)
+// {
+//     console.log("DrawBargraph: sample = ", sampleID);
+// }
 
 
-function DrawBubbleChart(sampleID)
-{
-    console.log("DrawBublleChart: sampleID ; ", sampleID);
-}
+// function DrawBubbleChart(sampleID)
+// {
+//     console.log("DrawBublleChart: sampleID ; ", sampleID);
+// }
 
-function ShowMetadata(sampleID)
-{
-    console.log("ShowMetadata: sampleID = ", sampleID);
-}
+// function ShowMetadata(sampleID)
+// {
+//     console.log("ShowMetadata: sampleID = ", sampleID);
+// }
 
-function optionChanged(newSampleID)
-{
-    ShowMetadata(newSampleID);
-    DrawBargraph(newSampleID);
-    DrawBubbleChart(newSampleID);
-    console.log("Draop Down changed to ", newSampleID);
-}
-
-
-
-// ** Initialize the screen with default information **
-// populate the dropdown with the list of ID's from the dataset
-
-function init()
-{
-    console.log("initializing screen");
-    var selector = d3.select("#selDataset");
+// function optionChanged(newSampleID)
+// {
+//     ShowMetadata(newSampleID);
+//     DrawBargraph(newSampleID);
+//     DrawBubbleChart(newSampleID);
+//     console.log("Draop Down changed to ", newSampleID);
+// }
 
 
-    sampleID = 100
 
-    DrawBargraph(sampleID);
-    DrawBubbleChart(sampleID);
-    ShowMetadata(sampleID);
-}
+// // ** Initialize the screen with default information **
+// // populate the dropdown with the list of ID's from the dataset
 
-init();
+// function init()
+// {
+//     console.log("initializing screen");
+//     var selector = d3.select("#selDataset");
+
+//     d3.json("samples.json"), then((data) => {
+//         var sampleName = data.names;
+
+//         sampleNames.forEach((sample) => {
+//             selector
+//                 .append("option")
+//                 .text(sample)
+//                 .property("value", sample);
+//         })
+//     })
+
+//     sampleID = 100
+
+//     DrawBargraph(sampleID);
+//     DrawBubbleChart(sampleID);
+//     ShowMetadata(sampleID);
+// }
+
+// init();
