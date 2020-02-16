669811567d872f577533144093cc6e6e1fcf9921
ErinMandell <ErinMandell@users.noreply.github.com>
2020-02-15 18:07:20 -0600

    Bubble Chart added

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -1,4 +1,105 @@
 
+function DrawBubbleChart(selectedSampleID)
+{
+    console.log("DrawBubbleChart: sample =  ", selectedSampleID);
+
+    d3.json("samples.json").then((data) => {
+
+        var samples = data.samples;
+        // console.log("All Samples: ", samples);
+
+        var resultArray = samples.filter(sampleObj => sampleObj.id == selectedSampleID);
+        // console.log("resultArray for selectedSampleID: ", resultArray);
+
+        var result = resultArray[0];
+        // console.log(result);
+
+        var otu_ids = result.otu_ids;
+        // console.log("otu_ids: ", otu_ids);
+
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+
+
+        var trace = {
+            x: otu_ids,
+            y: sample_values,
+            // text: sample_values,
+            mode: 'markers',
+            marker: {
+                color: otu_ids, 
+                opacity: .5,
+                size: sample_values
+            }
+        };
+    
+        var data = [trace];
+
+        var layout = {
+            title: 'All Bacteria Samples, by ID',
+            xaxis: {
+                title: 'OTU ID'
+            },
+            yaxis: {
+                title: 'Bacteria Count'
+            },
+            showlegend: false,
+            height: 600,
+            width: 1200
+        };
+
+        Plotly.newPlot('bubble', data, layout);
+    });
+}
+
+
+function DrawGaugeChart(sampleID)
+{
+    console.log("DrawBubbleChart: sample =  ", sampleID);
+
+
+}
+
+
+function ShowMetadata(selectedSampleID)
+{
+    console.log("ShowMetadata: sample = ", selectedSampleID);
+    
+    d3.json("samples.json").then((data) => {
+
+        d3.select("#sample-metadata").selectAll("tr").remove();
+
+        var metadata = data.metadata;
+        // console.log("All Metadata: ", metadata);
+
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == selectedSampleID);
+        console.log("Metadata result for selected ID: ", resultArray);
+        result = resultArray[0];
+
+        var id = result.id;
+        var ethnicity = result.ethnicity;
+        console.log(ethnicity);
+        var gender = result.gender;
+        var age = result.age;
+        var location = result.location;
+        var bbtype = result.bbtype;
+        var washfreq = result.wfreq;
+
+        var tbody = d3.select("#sample-metadata");
+        var trow;
+        trow = tbody.append("tr");
+        trow.append("tr").text(`Subject ID:  ${id}`);
+        trow.append("tr").text(`Ethnicity:  ${ethnicity}`);
+        trow.append("tr").text(`Gender:  ${gender}`);
+        trow.append("tr").text(`Age:  ${age}`);
+        trow.append("tr").text(`Location:  ${location}`);
+        trow.append("tr").text(`Belly Button Type:  ${bbtype}`);
+        trow.append("tr").text(`Wash Frequency:  ${washfreq}`);
+    });
+}
+
+
 function DrawBargraph(selectedSampleID)
 {
     console.log("DrawBargraph: sample = ", selectedSampleID);
@@ -43,50 +144,6 @@ function DrawBargraph(selectedSampleID)
     });
 }
 
-
-function DrawBubbleChart(sampleID)
-{
-    console.log("DrawBubbleChart: sample =  ", sampleID);
-}
-
-
-function ShowMetadata(selectedSampleID)
-{
-    console.log("ShowMetadata: sample = ", selectedSampleID);
-    
-    d3.json("samples.json").then((data) => {
-
-        var metadata = data.metadata;
-        // console.log("All Metadata: ", metadata);
-
-        var resultArray = metadata.filter(sampleObj => sampleObj.id == selectedSampleID);
-        console.log("Metadata result for selected ID: ", resultArray);
-        result = resultArray[0];
-
-        var id = result.id;
-        var ethnicity = result.ethnicity;
-        console.log(ethnicity);
-        var gender = result.gender;
-        var age = result.age;
-        var location = result.location;
-        var bbtype = result.bbtype;
-        var washfreq = result.wfreq;
-
-        var tbody = d3.select("#sample-metadata");
-        // var tbody = table.select("panel-body");
-        var trow;
-        trow = tbody.append("tr");
-        trow.append("tr").text(`Subject ID:  ${id}`);
-        trow.append("tr").text(`Ethnicity:  ${ethnicity}`);
-        trow.append("tr").text(`Gender:  ${gender}`);
-        trow.append("tr").text(`Age:  ${age}`);
-        trow.append("tr").text(`Location:  ${location}`);
-        trow.append("tr").text(`Belly Button Type:  ${bbtype}`);
-        trow.append("tr").text(`Wash Frequency / Wk:  ${washfreq}`);
-    });
-}
-
-
 // create 'optionChanged' function which is referenced in the index line 27.
 // This will be the ID that the user has selected
 
@@ -100,6 +157,7 @@ function optionChanged(newSampleID)
     ShowMetadata(newSampleID);
     DrawBargraph(newSampleID);
     DrawBubbleChart(newSampleID);
+    DrawGaugeChart(sampleID);
 }
 
 
@@ -130,6 +188,7 @@ function Init() {
     DrawBargraph(sampleID);
     DrawBubbleChart(sampleID);
     ShowMetadata(sampleID);
+    DrawGaugeChart(sampleID);
 }
 
 Init();
