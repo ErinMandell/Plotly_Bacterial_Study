4c9240422347466b99bffae7de6b4386b65f17f6
ErinMandell <ErinMandell@users.noreply.github.com>
2020-02-15 18:59:37 -0600

    delete old plots file,  restructure app.js

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -1,67 +1,57 @@
 
-function DrawBubbleChart(selectedSampleID)
-{
-    console.log("DrawBubbleChart: sample =  ", selectedSampleID);
+// function DrawBubbleChart(selectedSampleID)
+// {
+//     console.log("DrawBubbleChart: sample =  ", selectedSampleID);
 
-    d3.json("samples.json").then((data) => {
+//     d3.json("samples.json").then((data) => {
 
-        var samples = data.samples;
-        // console.log("All Samples: ", samples);
+//         var samples = data.samples;
+//         var resultArray = samples.filter(sampleObj => sampleObj.id == selectedSampleID);
+//         var result = resultArray[0];
 
-        var resultArray = samples.filter(sampleObj => sampleObj.id == selectedSampleID);
-        // console.log("resultArray for selectedSampleID: ", resultArray);
-
-        var result = resultArray[0];
-        // console.log(result);
-
-        var otu_ids = result.otu_ids;
-        // console.log("otu_ids: ", otu_ids);
-
-        var otu_labels = result.otu_labels;
-        var sample_values = result.sample_values;
+//         var otu_ids = result.otu_ids;
+//         var otu_labels = result.otu_labels;
+//         var sample_values = result.sample_values;
 
 
 
-        var trace = {
-            x: otu_ids,
-            y: sample_values,
-            text: otu_labels,
-            // text: sample_values,
-            mode: 'markers',
-            marker: {
-                color: otu_ids, 
-                opacity: .5,
-                size: sample_values
-            }
-        };
+//         var bubbleTrace = {
+//             x: otu_ids,
+//             y: sample_values,
+//             text: otu_labels,
+//             mode: 'markers',
+//             marker: {
+//                 color: otu_ids, 
+//                 opacity: .5,
+//                 size: sample_values
+//             }
+//         };
     
-        var data = [trace];
+//         var bubbleData = [bubbleTrace];
 
-        var layout = {
-            title: 'All Bacteria Samples, by ID',
-            xaxis: {
-                title: 'OTU ID'
-            },
-            yaxis: {
-                title: 'Bacteria Count'
-            },
-            showlegend: false,
-            height: 600,
-            width: 1200
-        };
+//         var bubbleLayout = {
+//             title: 'All Bacteria Samples, by ID',
+//             xaxis: {
+//                 title: 'OTU ID'
+//             },
+//             yaxis: {
+//                 title: 'Bacteria Count'
+//             },
+//             showlegend: false,
+//             height: 600,
+//             width: 1200
+//         };
 
-        Plotly.newPlot('bubble', data, layout);
-    });
-}
-
-
-function DrawGaugeChart(sampleID)
-{
-    console.log("DrawBubbleChart: sample =  ", sampleID);
+//         Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+//     });
+// }
 
 
-}
 
+
+// **************************************************************************
+// *****   Define function to Show the Metadata for the selected Sample ID
+// **************************************************************************
 
 function ShowMetadata(selectedSampleID)
 {
@@ -80,7 +70,6 @@ function ShowMetadata(selectedSampleID)
 
         var id = result.id;
         var ethnicity = result.ethnicity;
-        console.log(ethnicity);
         var gender = result.gender;
         var age = result.age;
         var location = result.location;
@@ -101,28 +90,31 @@ function ShowMetadata(selectedSampleID)
 }
 
 
-function DrawBargraph(selectedSampleID)
+// ****************************************************************************
+// *******        Define Function to fetch data, assign variables,       ******
+// *******   and draw Bar + Bubble Charts illustrating bacteria counts   ******
+// ****************************************************************************
+
+function DrawBarBubble(selectedSampleID)
 {
     console.log("DrawBargraph: sample = ", selectedSampleID);
 
     d3.json("samples.json").then((data) => {
 
+        // ******************************************************************
+        // Fetch data variables for the Sample ID that that the user selected
+
         var samples = data.samples;
-        // console.log("All Samples: ", samples);
-
-        // filter all of the sample ID objects to find the one that matches the selected Sample ID
-        
         var resultArray = samples.filter(sampleObj => sampleObj.id == selectedSampleID);
-        // console.log("resultArray for selectedSampleID: ", resultArray);
-
         var result = resultArray[0];
-        // console.log(result);
 
         var otu_ids = result.otu_ids;
-        // console.log("otu_ids: ", otu_ids);
-
         var otu_labels = result.otu_labels;
         var sample_values = result.sample_values;
+
+
+        // *****************************************************************
+        // *** Draw Bar Chart showing Top 10 bacteria counts for this Sample ID
 
         var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
 
@@ -136,36 +128,87 @@ function DrawBargraph(selectedSampleID)
             }
         ];
     
-        var varLayout = {
+        var barLayout = {
             title: "Top 10 Bacteria Cultures Found",
             margin: {t: 30, l: 150}
         };
 
-        Plotly.newPlot("bar", barData, varLayout);
+        Plotly.newPlot("bar", barData, barLayout);
+
+
+        // *****************************************************************
+        // *** Draw Bubble Chart showing bacteria counts for this Sample ID
+
+        var bubbleTrace = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+                color: otu_ids, 
+                opacity: .5,
+                size: sample_values
+            }
+        };
+    
+        var bubbleData = [bubbleTrace];
+
+        var bubbleLayout = {
+            title: 'All Bacteria Samples, by ID',
+            xaxis: {
+                title: 'OTU ID'
+            },
+            yaxis: {
+                title: 'Bacteria Count'
+            },
+            showlegend: false,
+            height: 600,
+            width: 1200
+        };
+
+        Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+
     });
 }
 
-// create 'optionChanged' function which is referenced in the index line 27.
-// This will be the ID that the user has selected
+
+
+
+function DrawGaugeChart(selectedSampleID)
+{
+    console.log("DrawGaugeChart: sample =  ", selectedSampleID);
+
+
+
+    
+
+
+}
+
+
+// *************************************************************************
+// ****    Create 'optionChanged' function                       ***********
+// ****     - This function is called from the event listener    ***********
+// ****       attached to the dropdown selector in index.html    ***********
+// ****   - 'newSampleID' is the ID that the user has selected   ***********
+// *************************************************************************
 
 function optionChanged(newSampleID)
 {
-    // buildCharts(newSample);
-    // buldMetadata(newSample);
-
     console.log("Dropdown changed to: ", newSampleID);
 
     ShowMetadata(newSampleID);
-    DrawBargraph(newSampleID);
-    DrawBubbleChart(newSampleID);
-    DrawGaugeChart(sampleID);
+    DrawBarBubble(newSampleID);
+    DrawGaugeChart(newSampleID);
 }
 
 
 
-// ** Initialize the screen with default information **
-// populate the dropdown with the list of ID's from the dataset
-// Do this by 
+
+// **********************************************************************
+// ******  Initialize screen with default Charts + information      *****
+// ******   and populate dropdown selector with list of Sample IDs  *****
+// ********************************************************************** 
 
 function Init() {
     console.log("Initializing Screen");
@@ -176,7 +219,6 @@ function Init() {
 
     d3.json("samples.json").then((data) => {
         var sampleNames = data.names;
-        // console.log("Init SampleNames: ", sampleNames);
 
         sampleNames.forEach((sampleID) => {
             selector
@@ -186,8 +228,7 @@ function Init() {
         });
     });
 
-    DrawBargraph(sampleID);
-    DrawBubbleChart(sampleID);
+    DrawBarBubble(sampleID);
     ShowMetadata(sampleID);
     DrawGaugeChart(sampleID);
 }
